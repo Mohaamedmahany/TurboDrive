c1ce0a7ff5a8ab0e6308638508a73115f6bd0f66
Mohamed Mohany <mohamedmohany018@gmail.com>
2025-11-29 22:36:21 +0200

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",           
+  reactCompiler: true,       
+  basePath: "/TurboDrive",    
+  assetPrefix: "/TurboDrive/" 
 };
 
-export default nextConfig;
+module.exports = nextConfig;
