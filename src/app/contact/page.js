2d1c7c91e8033105484d2c27f90ef72984772adc
Mohamed Mohany <mohamedmohany018@gmail.com>
2025-11-30 01:19:13 +0200

    Updated section layout

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -11,7 +11,7 @@
         <div className="container flex flex-col items-center justify-between w-full gap-10 p-6 bg-gray-900 rounded-md lg:flex-row">
           {/* --- INFO  --- */}
           <div className="flex flex-col justify-center w-full text-white info lg:w-1/2">
-            <h3 className="p-1 font-bold lg:text-3xl">
+            <h3 className="p-1 font-bold lg:text-[32px]">
               We are here to help !
             </h3>
 
